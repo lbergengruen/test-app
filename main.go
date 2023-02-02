29110bbeddabbfd7e6873eaec39144daf755a7f8
lbergengruen <lbergengruen@roku.com>
2023-02-02 09:56:04 -0300

    Changed APp

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), func(opts *config.LoadOptions) error {
+		opts.Region = "us-east-1"
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+	p := dynamodb.NewListTablesPaginator(svc, nil, func(o *dynamodb.ListTablesPaginatorOptions) {
+		o.StopOnDuplicateToken = true
+	})
+
+	for p.HasMorePages() {
+		out, err := p.NextPage(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+
+		for _, tn := range out.TableNames {
+			fmt.Println(tn)
+		}
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"context"
-	"github.com/stretchr/testify/assert"
-	tc "github.com/testcontainers/testcontainers-go"
-	"strconv"
+	"fmt"
 	"testing"
-	"time"
 )
 
-func TestMain(t *testing.T) {
-	uid := strconv.FormatInt(time.Now().UnixMilli(), 10)
-
-	compose, err := tc.NewDockerCompose("./testresources/docker-compose.yml")
-	assert.NoError(t, err, "NewDockerComposeAPI()")
-
-	t.Cleanup(func() {
-		assert.NoError(t, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
-	})
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-
-	err = compose.WithEnv(map[string]string{"uid": uid}).Up(ctx, tc.Wait(true))
-	assert.NoError(t, err, "compose.Up()")
+func TestMain(m *testing.M) {
+	fmt.Println("Hello Test!")
 }
